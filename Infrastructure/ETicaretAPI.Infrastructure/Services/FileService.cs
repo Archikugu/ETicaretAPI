d0612a282742587912515da00fb559da11ddd7aa
Gokhan <engokhangok@gmail.com>
2024-07-04 23:30:23 +0300

    SEO-Friendly Renaming of File Names Added

--- Infrastructure/ETicaretAPI.Infrastructure/Services/FileService.cs
+++ Infrastructure/ETicaretAPI.Infrastructure/Services/FileService.cs
@@ -1,8 +1,10 @@
 ﻿using ETicaretAPI.Application.Services;
+using ETicaretAPI.Infrastructure.Operations;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,22 +25,50 @@ namespace ETicaretAPI.Infrastructure.Services
             try
             {
                 await using FileStream fileStream = new(path, FileMode.Create, FileAccess.Write, FileShare.None, 1024 * 1024, false);
-
-                await fileStream.CopyToAsync(fileStream);
-
+                await file.CopyToAsync(fileStream);
                 await fileStream.FlushAsync();
                 return true;
             }
             catch (Exception ex)
             {
-                // toDo log!!
-                throw ex;
+                // Log the exception (to be implemented)
+                throw;
             }
         }
 
-        public Task<string> FileRenameAsync(string fileName)
+        private async Task<string> FileRenameAsync(string path, string fileName, bool first = true)
         {
-            throw new NotImplementedException();
+            return await Task.Run(() =>
+            {
+                string extension = Path.GetExtension(fileName);
+                string baseFileName = Path.GetFileNameWithoutExtension(fileName);
+                string newFileName = first ? $"{NameOperation.CharacterRegulatory(baseFileName)}{extension}" : fileName;
+
+                while (File.Exists(Path.Combine(path, newFileName)))
+                {
+                    int dashIndex = newFileName.LastIndexOf('-');
+                    int dotIndex = newFileName.LastIndexOf('.');
+
+                    if (dashIndex == -1 || dashIndex > dotIndex)
+                    {
+                        newFileName = $"{baseFileName}-2{extension}";
+                    }
+                    else
+                    {
+                        string numberPart = newFileName.Substring(dashIndex + 1, dotIndex - dashIndex - 1);
+                        if (int.TryParse(numberPart, out int fileNumber))
+                        {
+                            newFileName = $"{baseFileName}-{fileNumber + 1}{extension}";
+                        }
+                        else
+                        {
+                            newFileName = $"{baseFileName}-2{extension}";
+                        }
+                    }
+                }
+
+                return newFileName;
+            });
         }
 
         public async Task<List<(string fileName, string path)>> UploadAsync(string path, IFormFileCollection files)
@@ -52,23 +82,18 @@ namespace ETicaretAPI.Infrastructure.Services
 
             foreach (IFormFile file in files)
             {
-                string fileNewName = await FileRenameAsync(file.FileName);
-
-                bool result = await CopyFileAsync($"{uploadPath}\\{fileNewName}", file);
-
-                datas.Add((fileNewName, $"{uploadPath}\\{fileNewName}"));
-
+                string fileNewName = await FileRenameAsync(uploadPath, file.FileName);
+                bool result = await CopyFileAsync(Path.Combine(uploadPath, fileNewName), file);
+                datas.Add((fileNewName, Path.Combine(uploadPath, fileNewName)));
                 results.Add(result);
-
             }
-            if (results.TrueForAll(x => x.Equals(true)))
+
+            if (results.TrueForAll(x => x))
             {
                 return datas;
             }
 
-            //toDo if geçerli değilse buradaki dosyaların hata alındağına dair exception yaz
             return null;
         }
-
     }
 }
